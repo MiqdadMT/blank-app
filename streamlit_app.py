a99068f52efbd14e499cc6e2d98566d4d4444927
MiqdadMT <miqdadmt02@gmail.com>
2025-05-27 11:15:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("miqdad cinta zahra")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "miqdad pengen kawin"
 )
